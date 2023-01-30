0f43db688285fade8e9dca8679ceb2b31d9a287c
Sizimon <siizmonsus@gmail.com>
2023-01-30 22:43:52 +0000

    fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -35,7 +35,7 @@ DEBUG = development
 if development:
     ALLOWED_HOSTS = ['localhost']
 else:
-    ALLOWED_HOSTS = 'codestar-shhmon.herokuapp.com'
+    ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME')]
 
 
 # Application definition
