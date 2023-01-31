09c0aba93da8546fc69811980ef6ef57050b63b0
Sizimon <siizmonsus@gmail.com>
2023-01-31 21:46:02 +0000

    Final fix

--- codestar/settings.py
+++ codestar/settings.py
@@ -30,6 +30,8 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
+X_FRAME_OPTIONS = 'SAMEORIGIN'
+
 
 ALLOWED_HOSTS = [os.environ.get('HEROKU_HOSTNAME'), 'localhost']
 
